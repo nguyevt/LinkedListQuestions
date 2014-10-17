b13bd339e4a58d958a7b1097633f3632eac8fb23
Vincent Nguyen <vincentnguyen1732@gmail.com>
2014-10-17 16:56:02 -0700

    Adding a simple implementation of a queue

--- src/questions/QueueImplementation.java
+++ src/questions/QueueImplementation.java
@@ -1,17 +1,40 @@
 package questions;
 
+import linkedListImplementation.LinkedIntListNode;
+
 public class QueueImplementation {
 	
-	//Constructor
+	LinkedIntListNode front;
+	
+	public QueueImplementation() {
+		this.front = null;
+	}
 	
 	//Peek
+	public int peek() {
+		return front.getValue();
+	}
 	
 	//Remove
+	public int remove() {
+		int value = front.getValue();
+		front = front.getNext();
+		return value;
+	}
 	
 	//Add
+	public void add(int value) {
+		this.front = new LinkedIntListNode(value, front);
+	}
 	
 	//IsEmpty
+	public boolean isEmpty() {
+		return front == null;
+	}
 	
 	//Clear
+	public void clear() {
+		front = null;
+	}
 	
 }
